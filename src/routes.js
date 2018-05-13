a3335a7c8a6f25b8e5f14993afa9e5dd42b0a84e
Allan Carvalho <allan.carvalho@b2wdigital.com>
2018-05-13 00:27:41 -0300

    WIP

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,7 @@
 import {asyncRoute} from 'all-in-one';
-import Home from './pages/home';
 
-const About = asyncRoute(() => import('./pages/about'));
+import Home from './pages/home'; //rota no js principal
+const About = asyncRoute(() => import('./pages/about')); //rota em bundle separado
 
 const routes = [
   { path: '/',
